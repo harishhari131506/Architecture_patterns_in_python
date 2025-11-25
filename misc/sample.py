c4ac5c57f77eba90508cb2850ff07dc56cc42b76
harishhari131506 <harish130615@gmail.com>
2025-11-25 07:45:53 +0530

    abstraction understanding

--- misc/sample.py
+++ misc/sample.py
@@ -1,15 +1,36 @@
-import json
-from urllib.request import urlopen
-from urllib.parse import urlencode
+from duckduckgo_search import DDGS
 
-params = dict(q='king',format="json")
-handle = urlopen('https://api.duckduckgo.com' + '?' + urlencode(params))
+with DDGS() as ddgs:
+    results = ddgs.text("King")
+    for r in results:
+        print(r["href"] + " - " + r["body"])
 
-raw_text = handle.read().decode('utf-8')
-parsed = json.loads(raw_text)
 
-results = parsed['RelatedTopics']
-print(results)
-for r in results:
-    if isinstance(r, dict) and "Text" in r:
-        print(f"{r['FirstUrl']} - {r['Text']}")
+# import requests
+
+# params = dict(q='King' , format='json')
+# parsed = requests.get('https://api.duckduckgo.com' , params=params).json()
+
+# results = parsed['RelatedTopics']
+
+# for r in results:
+#     if 'Text' in r:
+#         print(f"{r['FirstURL']} - {r['Text']}")
+
+
+# import json
+# from urllib.request import urlopen
+# from urllib.parse import urlencode
+
+# params = dict(q='king',format="json")
+# handle = urlopen('https://api.duckduckgo.com' + '?' + urlencode(params))
+
+# raw_text = handle.read().decode('utf-8')
+# parsed = json.loads(raw_text)
+
+# results = parsed['RelatedTopics']
+
+# for r in results:
+#     if isinstance(r, dict) and "Text" in r:
+#         print(f"{r['FirstURL']} - {r['Text']}")
+#         print("------")
